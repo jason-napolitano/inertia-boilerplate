12c0452ee894da13edbd4a6bdd9f376a8dc8fada
jason-napolitano <jnapolitanoit@gmail.com>
2025-03-25 10:36:11 -0700

    Test cleanup

--- resources/vue/Composables/__tests__/useNumber.test.ts
+++ resources/vue/Composables/__tests__/useNumber.test.ts
@@ -1,25 +1,32 @@
+import { expect, test, describe } from 'vitest'
 import { useNumber } from '../useNumber'
-import { expect, test } from 'vitest'
 
 // composables
 const number = useNumber()
 
-test('converts a number to a thousandth string', function () {
-  expect(number.thousandth(1000)).toBe('1,000')
-})
+describe('number composables', () => {
+  // ----------------------------------------------------
+  test('converts a number to a thousandth string', function () {
+    expect(number.thousandth(1000)).toBe('1,000')
+  })
 
-test('converts a number to a currency string', function () {
-  expect(number.currency(100.25)).toBe('100.25')
-})
+  // ----------------------------------------------------
+  test('converts a number to a currency string', function () {
+    expect(number.currency(100.25)).toBe('100.25')
+  })
 
-test('converts a number to a percentage', function () {
-  expect(number.percentage(250, 500)).toBe(50.0)
-})
+  // ----------------------------------------------------
+  test('converts a number to a percentage', function () {
+    expect(number.percentage(250, 500)).toBe(50.0)
+  })
 
-test('evaluates if x is greater than y', function () {
-  expect(number.gt(10, 1)).toBeTruthy()
-})
+  // ----------------------------------------------------
+  test('evaluates if x is greater than y', function () {
+    expect(number.gt(10, 1)).toBeTruthy()
+  })
 
-test('evaluates if x is less than y', function () {
-  expect(number.lt(1, 10)).toBeTruthy()
+  // ----------------------------------------------------
+  test('evaluates if x is less than y', function () {
+    expect(number.lt(1, 10)).toBeTruthy()
+  })
 })
--- resources/vue/Composables/__tests__/useString.test.ts
+++ resources/vue/Composables/__tests__/useString.test.ts
@@ -1,16 +1,66 @@
+import { expect, test, describe } from 'vitest'
 import { useString } from '../useString'
-import { expect, test } from 'vitest'
 
 // composable
 const string = useString()
 
-// variables
-let original: string = 'stair'
-let modified: string = 'stairs'
+describe('string composables', () => {
+  // ----------------------------------------------------
+  test('returns pluralized version of a word', () => {
+    expect(string.plural('stair')).toBe('stairs')
+  })
 
-// tests
-test('returns pluralized version of a word', () => {
-  original = 'stair'
-  modified = 'stairs'
-  expect(string.plural(original)).toBe(modified)
+  // ----------------------------------------------------
+  test('returns a truncated version of a string', () => {
+    const original = 'This is a trimmed string. We expect it to be different'
+    expect(string.truncate(original, 35)).toBe(
+      'This is a trimmed string. We expect...'
+    )
+  })
+
+  // ----------------------------------------------------
+  test('removes hyphens from a string', () => {
+    const original = 'this-will-have-the-hyphens-removed'
+    expect(string.removeHyphens(original)).toBe(
+      'this will have the hyphens removed'
+    )
+  })
+
+  // ----------------------------------------------------
+  test('converts a string to title case', () => {
+    const original = 'This string will be title cased'
+    expect(string.toTitleCase(original)).toBe('This String Will Be Title Cased')
+  })
+
+  // ----------------------------------------------------
+  test('capitalizes the first word in a string', () => {
+    expect(string.ucFirst('world')).toBe('World')
+  })
+
+  // ----------------------------------------------------
+  test('converts a string to a slug', () => {
+    const original = 'This string will be slugged'
+    expect(string.slug(original)).toBe('this-string-will-be-slugged')
+  })
+
+  // ----------------------------------------------------
+  test('returns a pluralized version of a word', () => {
+    expect(string.plural('stair')).toBe('stairs')
+  })
+
+  // ----------------------------------------------------
+  test('returns a singular version of a word', () => {
+    expect(string.singular('stairs')).toBe('stair')
+  })
+
+  // ----------------------------------------------------
+  test('returns a human-readable phone number', () => {
+    const phone = '800-555-1234'
+    expect(string.phoneNumber(phone)).toBe('(800) 555-1234')
+  })
+
+  // ----------------------------------------------------
+  test('returns initials for two words in a string', () => {
+    expect(string.initials('Hello World')).toBe('HW')
+  })
 })
